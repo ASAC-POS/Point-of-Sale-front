63ff505883807b721d2a3a9f650f1dee598a3265
Mhsalameh <mohsalameh1@gmail.com>
2022-06-20 18:32:22 +0300

    getting products from the api

--- pos/src/components/login/Login.js
+++ pos/src/components/login/Login.js
@@ -1,17 +1,24 @@
 import { Form, Button } from 'react-bootstrap';
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import { loginContext } from '../../context/context.js';
 import { Link } from 'react-router-dom';
-function Login() {
+import { getProductsFromAPI } from '../../store/products';
+import { connect } from 'react-redux';
+function Login(props) {
   const { login } = useContext(loginContext);
+  const { products, getProductsFromAPI } = props;
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  useEffect(() => {
+    console.log(products);
+  }, [products]);
   return (
     <div>
       <Form
         onSubmit={(e) => {
           e.preventDefault();
           login(username, password);
+          // getProductsFromAPI();
         }}
       >
         <Form.Group className='mb-3' controlId='formBasicEmail'>
@@ -42,4 +49,10 @@ function Login() {
   );
 }
 
-export default Login;
+const mapStateToProps = (state) => ({
+  products: state.products.products,
+});
+
+const mapDispatchToProps = { getProductsFromAPI };
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
--- pos/src/index.js
+++ pos/src/index.js
@@ -10,12 +10,12 @@ import LoginProvider from './context/context';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <LoginProvider>
-      <BrowserRouter>
-        <Provider store={store}>
+    <BrowserRouter>
+      <Provider store={store}>
+        <LoginProvider>
           <App />
-        </Provider>
-      </BrowserRouter>
-    </LoginProvider>
+        </LoginProvider>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
