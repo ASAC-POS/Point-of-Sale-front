68f0ebdb22a09ae05066e372326ba3472ebc2da1
Neveen97 <neveenaburomman1@gmail.com>
2022-06-23 15:10:47 +0300

    checkoutform added

--- pos/src/components/Products/Pos.js
+++ pos/src/components/Products/Pos.js
@@ -1,5 +1,31 @@
 import React from 'react';
+import CheckoutForm from './CheckoutForm/CheckoutForm'
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+import Modal from 'react-modal';
+import { AbsoluteCenter, Center } from '@chakra-ui/react';
 
 export default function Pos() {
-  return <div>Pos</div>;
+
+  const [modalIsOpen,setModalIsOpen] = useState(false);
+
+  const setModalIsOpenToTrue =()=>{
+    setModalIsOpen(true)
+}
+
+const setModalIsOpenToFalse =()=>{
+    setModalIsOpen(false)
+}
+    
+
+  return (
+  <div>
+   <h1>Pos Page</h1>
+   <Button onClick={setModalIsOpenToTrue} size="small" color="primary">  checkout  </Button>
+   <Modal isOpen={modalIsOpen}  ariaHideApp={false} >
+  <button onClick={setModalIsOpenToFalse}> close </button>
+                <CheckoutForm/> 
+            </Modal>
+    </div>
+    );
 }
