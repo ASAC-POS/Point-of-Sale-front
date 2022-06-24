5c0e2dae97a0e08bb3a09ce50f5445217fe60da0
leenahmad <leenkaraja24@gmail.com>
2022-06-25 00:17:01 +0300

    edit pos page

--- pos/src/components/Products/Pos.js
+++ pos/src/components/Products/Pos.js
@@ -5,7 +5,7 @@ import Button from 'react-bootstrap/Button';
 import { useState } from 'react';
 import Modal from 'react-modal';
 import InfoScreen from './InfoScreen';
-import Row  from "react-bootstrap/Col";
+
 
 import { connect } from 'react-redux';
 
@@ -20,7 +20,7 @@ function Pos(props) {
     setModalIsOpen(false);
   };
   return (
-    <Row >
+   
     <div
       style={{
         display: 'flex',
@@ -68,7 +68,6 @@ function Pos(props) {
       </div>
       <InfoScreen />
     </div>
-    </Row >
   );
 }
 
