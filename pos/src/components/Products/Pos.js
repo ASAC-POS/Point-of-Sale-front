1386f200292839621f7580eba13c262d3f15e44b
JalalHasan-22 <jhasanj8@gmail.com>
2022-06-29 02:29:41 +0300

    final style

--- pos/src/components/Products/Pos.js
+++ pos/src/components/Products/Pos.js
@@ -75,66 +75,66 @@ function Pos(props) {
               </Card>
             );
           })}
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column-reverse',
+            justifyContent: 'center',
+            width: '100vw',
+            height: '100%',
+          }}
+        >
+          {/* <div style={{ width: '50%' }}>
+            {products.map((product) => {
+              return (
+                <Card
+                  key={product.id}
+                  style={{ width: '18rem', height: '250px' }}
+                >
+                  <Card.Body>
+                    <Card.Title>{product.productName}</Card.Title>
+                    <Card.Text>{product.description}</Card.Text>
+                    <Card.Text>{product.price}$</Card.Text>
+                    <input
+                      type='number'
+                      min={1}
+                      placeholder='quantity'
+                      onChange={(e) => {
+                        setQuantity(parseInt(e.target.value));
+                      }}
+                    />
+                    {er && product.quantity === 0 && <p>{er}</p>}
+                    <Button
+                      onClick={() => {
+                        if (quantity > product.quantity) {
+                          setEr('out of stock');
+                        } else {
+                          // setEr(null);
+                          addItemToCheckout({
+                            name: product.productName,
+                            quantity: quantity,
+                            productID: product.id,
+                            price: product.price,
+                          });
+                          deductProduct({
+                            id: product.id,
+                            quantity: quantity,
+                          });
+                        }
+                      }}
+                      variant='primary'
+                    >
+                      Add{' '}
+                    </Button>
+                  </Card.Body>
+                </Card>
+              );
+            })}
+          </div> */}
+          <InfoScreen />
 
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'space-evenly',
-              width: '100vw',
-              height: '100%',
-            }}
-          >
-            <div style={{ width: '50%' }}>
-              {products.map((product) => {
-                return (
-                  <Card
-                    key={product.id}
-                    style={{ width: '18rem', height: '250px' }}
-                  >
-                    <Card.Body>
-                      <Card.Title>{product.productName}</Card.Title>
-                      <Card.Text>{product.description}</Card.Text>
-                      <Card.Text>{product.price}$</Card.Text>
-                      <input
-                        type='number'
-                        min={1}
-                        placeholder='quantity'
-                        onChange={(e) => {
-                          setQuantity(parseInt(e.target.value));
-                        }}
-                      />
-                      {er && product.quantity === 0 && <p>{er}</p>}
-                      <Button
-                        onClick={() => {
-                          if (quantity > product.quantity) {
-                            setEr('out of stock');
-                          } else {
-                            // setEr(null);
-                            addItemToCheckout({
-                              name: product.productName,
-                              quantity: quantity,
-                              productID: product.id,
-                              price: product.price,
-                            });
-                            deductProduct({
-                              id: product.id,
-                              quantity: quantity,
-                            });
-                          }
-                        }}
-                        variant='primary'
-                      >
-                        Add{' '}
-                      </Button>
-                    </Card.Body>
-                  </Card>
-                );
-              })}
-            </div>
-            <InfoScreen />
-
-            <ReceiptsForm />
-          </div>
+          <ReceiptsForm style={{ marginBottom: '10px' }} />
         </div>
       </div>
     </div>
