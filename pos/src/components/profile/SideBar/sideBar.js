03e2b7ec43f5967fbe01a2dcd074783058718343
Neveen97 <neveenaburomman1@gmail.com>
2022-06-28 19:23:04 +0300

    sideBar

--- pos/src/components/profile/SideBar/sideBar.js
+++ pos/src/components/profile/SideBar/sideBar.js
@@ -1,43 +1,13 @@
-import './SideBar/sidebar.css';
-import { useNavigate } from 'react-router-dom';
-import { connect } from 'react-redux';
-import cookie from 'react-cookies';
-import Auth from '../../context/auth';
-import { Card } from 'react-bootstrap';
-import { getPopupNotificationsFromAPI } from '../../store/popups';
-import { useContext, useEffect } from 'react';
-import { SocketContext } from '../../context/socket';
-import WhiteLogo from "../../assets/Bayya3-removebg-preview.png";
+import "./sidebar.css";
+import WhiteLogo from "../../../assets/Bayya3-removebg-preview.png";
 import { FcNext } from "react-icons/fc";
-import {BiExit} from "react-icons/bi";
+import {BiExit} from "react-icons/bi"
 
-import io from 'socket.io-client';
-import Popup from '../popup/Popup';
-
-function Profile(props) {
-  const { socket } = useContext(SocketContext);
-  const navigate = useNavigate();
-  const { store, getPopupNotificationsFromAPI } = props;
-
-  useEffect(() => {
-    socket.emit('reload-notifications');
-  }, []);
-
+export default function Sidebar() {
+    
   return (
     <>
-    {/*
-      <Card id='details'>
-        {/* <Card.Title
-          style={{ fontWeight: 'bold', alignSelf: 'center', marginTop: '20px' }}
-        >
-          USER INFORMATION
-        </Card.Title> 
-        <Card.Text>Store Name: {props.store?.storename}</Card.Text>
-        <Card.Text>User Name: {cookie.load('userData')?.username}</Card.Text>
-        <Card.Text>User Role: {cookie.load('userData')?.role}</Card.Text>
-      </Card>
-  */}
-     <div className="sidebar">
+      <div className="sidebar">
         <div className="logo">
           <img id="logo-img" src={WhiteLogo}></img>
           <h4 id="h44"> Bayya3</h4>
@@ -90,11 +60,3 @@ function Profile(props) {
     </>
   );
 }
-
-const mapStateToProps = (state) => ({
-  store: state.store.store,
-  popup: state.popup,
-});
-const mapDispatchToProps = { getPopupNotificationsFromAPI };
-
-export default connect(mapStateToProps, mapDispatchToProps)(Profile);
