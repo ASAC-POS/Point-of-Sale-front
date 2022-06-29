4835b0cd4db1425672e13fbb12489830288fd34f
JalalHasan-22 <jhasanj8@gmail.com>
2022-06-29 15:17:58 +0300

    products cards styled

--- pos/src/components/Products/ProductManage.js
+++ pos/src/components/Products/ProductManage.js
@@ -1,77 +1,71 @@
-import Card from "react-bootstrap/Card";
-import {BsFillArchiveFill } from "react-icons/bs"
-import Button from "react-bootstrap/Button";
-import "./banner.scss";
-import ProductsForm from "./ProductsForm";
-import EditProducts from "./EditProducts";
-import "./ProductManage.scss";
-import { deleteProduct } from "../../store/products";
-import { connect } from "react-redux";
-import Auth from "../../context/auth";
+import Card from 'react-bootstrap/Card';
+import { BsFillArchiveFill } from 'react-icons/bs';
+import Button from 'react-bootstrap/Button';
+import './banner.scss';
+import ProductsForm from './ProductsForm';
+import EditProducts from './EditProducts';
+import './ProductManage.scss';
+import { deleteProduct } from '../../store/products';
+import { connect } from 'react-redux';
+import Auth from '../../context/auth';
 
 function Product(props) {
   const { products } = props;
 
   return (
-    <div className="product-manage">
-      <div class="hpd hpd-basic">
-        <h4 class="hpd-title">Products Management</h4>
-        <p class="hpd-desc">
+    <div className='product-manage'>
+      <div class='hpd hpd-basic'>
+        <h4 class='hpd-title'>Products Management</h4>
+        <p class='hpd-desc'>
           freely add, edit, or remove a product as you see fit
         </p>
         <span class='hpd-btn'>
-        <Auth capability="edit">
+          <Auth capability='edit'>
             <ProductsForm />
           </Auth>
-          </span>
-        
+        </span>
       </div>
-      <div className="add-product">
-       
-      </div>
+      <div className='add-product'></div>
       <></>
-      <div className="products-card">
+      <div className='products-card'>
         {console.log(products)}
         {products.map((product) => {
-          
           return (
             <>
+              <Card className='product'>
+                <Card.Img src={product.imgURL} />
 
-              <Card style={{ width: "18rem" }} className="product">
+                <Card.Body className='details'>
+                  <div className='top-part'>
+                    <Card.Title className='card-title'>
+                      {product.productName}
+                    </Card.Title>
+                    <Card.Text>{product.price}$</Card.Text>
+                  </div>
 
-
-              <Card.Img
-              className="card-image"
-              variant="top"
-              // style={{ height: '220px' }}
-              src={product.imgURL} />
-
-                <Card.Body>
-                  <Card.Title>{product.productName}</Card.Title>
-                  <Card.Text>Product Name: {product.productName}</Card.Text>
-                  <Card.Text>Price: {product.price}$</Card.Text>
-                  <Card.Text>Description: {product.description}</Card.Text>
-                  <Card.Text>Quantity: {product.quantity}</Card.Text>
-                  <Card.Text>minQuantity: {product.minQuantity}</Card.Text>
-                  <Card.Footer className="product-button">
-                    <Auth capability="delete">
-
+                  <div className='card-description'>
+                    <Card.Text>{product.description}</Card.Text>
+                    <Card.Text>Quantity: {product.quantity}</Card.Text>
+                    <Card.Text>minQuantity: {product.minQuantity}</Card.Text>
+                  </div>
+                  <Card.Footer className='product-button'>
+                    <Auth capability='delete'>
                       <Button
-                        variant="danger"
-                        style={{ margin: "5%" }}
+                        variant='danger'
+                        style={{ margin: '5%' }}
                         onClick={() => props.deleteProduct(product.id)}
                       >
-                        <div className="remove">
+                        <div className='remove'>
                           <BsFillArchiveFill />
                         </div>
-
                       </Button>
                     </Auth>
 
                     <EditProducts id={product.id} />
                   </Card.Footer>
                 </Card.Body>
-              </Card></>
+              </Card>
+            </>
           );
         })}
       </div>
